06b4b799d13060265618f783b1ddfcac494c0f41
Sidrishe <xomikxam@mail.ru>
2024-06-18 20:23:02 +0300

    Кастомная команда для суперюзера

---
+++ users/management/commands/csu.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='test@mail.ru',
+            first_name='Mikhail',
+            last_name='Osip',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True
+        )
+
+        user.set_password('Qwerty123')
+        user.save()
